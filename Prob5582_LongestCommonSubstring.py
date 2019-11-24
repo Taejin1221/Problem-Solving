148194b3e1f0e4969ce5ab17be905d20c1b40ba6
Taejin <taejin7824@gmail.com>
2019-11-25 01:48:48 +0900

    Create Solution class and main

---
+++ Prob5582_LongestCommonSubstring.py
@@ -0,0 +1,14 @@
+# Prob5582_LongestCommonSubstring.py
+class Solution:
+	def LogestCommonSubstring(self, str1: str, str2: str) -> int:
+
+	@classmethod
+	def main(self):
+		str1 = input()
+		str2 = input()
+
+		rtrnVal = LogestCommonSubstring(str1, str2)
+
+		print(rtrnVal)
+
+Solution.main()
