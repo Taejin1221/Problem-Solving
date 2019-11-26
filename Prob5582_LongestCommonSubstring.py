a9641077a2b3fded1c3c6dad8dcad72ebca0c77c
Jin-Book <taejin7824@gmail.com>
2019-11-26 19:10:40 +0900

    Temp Solution

--- Prob5582_LongestCommonSubstring.py
+++ Prob5582_LongestCommonSubstring.py
@@ -1,14 +1,24 @@
 # Prob5582_LongestCommonSubstring.py
-class Solution:
-	def LogestCommonSubstring(self, str1: str, str2: str) -> int:
+str1 = input()
+str2 = input()
+# str1 = 'ABRACADABRA'
+# str2 = 'ECADADABRBCRDARA'
 
-	@classmethod
-	def main(self):
-		str1 = input()
-		str2 = input()
+def LogestCommonSubstring() -> int:
+	table = []
+	max1 = 0
 
-		rtrnVal = LogestCommonSubstring(str1, str2)
+	for i in range(len(str1)):
+		table.append([])
+		for j in range(len(str2)):
+			if ((i is 0) or (j is 0) or not(str1[i - 1] is str2[j - 1])):
+				table[i].append(0)
+			else:
+				table[i].append(table[i - 1][j - 1] + 1)
+				max1 = max(max1, table[i][j])
 
-		print(rtrnVal)
+	return max1
 
-Solution.main()
+rtrnVal = LogestCommonSubstring()
+
+print(rtrnVal)
