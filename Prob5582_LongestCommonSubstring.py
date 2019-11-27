171163543a13aa410bb29ed35a45c9f6ebb26ce7
Grape <grape@Grapeui-iMac.local>
2019-11-27 20:38:17 +0900

    Decomplete Solution with python3

--- Prob5582_LongestCommonSubstring.py
+++ Prob5582_LongestCommonSubstring.py
@@ -4,18 +4,24 @@ str2 = input()
 # str1 = 'ABRACADABRA'
 # str2 = 'ECADADABRBCRDARA'
 
+str1Size = len(str1)
+str2Size = len(str2)
+
 def LogestCommonSubstring() -> int:
-	table = []
+	table = [ [0 for i in range(str2Size)] for j in range(str1Size)]
 	max1 = 0
 
-	for i in range(len(str1)):
-		table.append([])
-		for j in range(len(str2)):
-			if ((i is 0) or (j is 0) or not(str1[i - 1] is str2[j - 1])):
-				table[i].append(0)
+	for i in range(str1Size):
+		for j in range(str2Size):
+			if ((i is 0) or (j is 0)):
+				table[i][j] = 0
+			elif (str1[i - 1] is str2[j - 1]):
+				table[i][j] = table[i - 1][j - 1] + 1
+
+				if (max1 < table[i][j]):
+					max1 = table[i][j]
 			else:
-				table[i].append(table[i - 1][j - 1] + 1)
-				max1 = max(max1, table[i][j])
+				table[i][j] = 0
 
 	return max1
 
