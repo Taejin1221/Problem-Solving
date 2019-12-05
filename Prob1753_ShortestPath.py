a2879584c636733601ff29f6b929570ac9c79a77
Jin-Book <taejin7824@gmail.com>
2019-12-05 16:46:40 +0900

    Temp Code Dijkstra Algorithm

--- Prob1753_ShortestPath.py
+++ Prob1753_ShortestPath.py
@@ -1,9 +1,11 @@
 # Prob1753_ShortestPath.py
+from math import inf
+
 class Graph():
 	VISIT = 1
 	UNVISIT = 0
-	vertex = 0
-	weight = 1
+	V = 0
+	W = 1
 
 	def __init__(self, vertexSize):
 		self.adjList = { vertex:[] for vertex in range(1, vertexSize + 1) }
@@ -19,8 +21,28 @@ class Graph():
 			print('V({}) = {}'.format(curr, self.adjList[curr]))
 
 	def DijkstraAlgorithm(self, start):
-		visited = { vertex: Graph.UNVISIT for vertex in range(1, self.vertexSize + 1) }
-		shortestPath = { vertex: inf for vertex in range(1, self.vertexSize + 1) }
+		visited = { vertex: Graph.UNVISIT \
+		for vertex in range(1, self.vertexSize + 1) }
+		shortestPath = { vertex: inf \
+		for vertex in range(1, self.vertexSize + 1) }
+
+		visited[start], shortestPath[start] = Graph.VISIT, 0
+		for i in range(self.vertexSize - 1):
+			minPath = None
+			for currVertex in self.adjList:
+				if (visited[currVertex] is Graph.VISIT):
+					for adjVertex in self.adjList[currVertex]:
+						tempWeight = shortestPath[currVertex] + adjVertex[Graph.W]
+						if (shortestPath[adjVertex[Graph.V]] > tempWeight):
+							shortestPath[adjVertex[Graph.V]] = tempWeight
+							
+						if ((minPath is None) or \
+							(shortestPath[minPath] > shortestPath[adjVertex[Graph.V]])):
+							minPath = shortestPath[adjVertex[Graph.V]]
+			visited[minPath] = Graph.VISIT
+
+		for short in shortestPath:
+			print(short)
 
 if (__name__ == '__main__'):
 	vertexSize, edgeSize = map(int, input().split())
@@ -31,5 +53,7 @@ if (__name__ == '__main__'):
 	for _ in range(edgeSize):
 		u, v, w = map(int, input().split())
 		myGraph.InsertEdge(u, v, w)
+
+	myGraph.DijkstraAlgorithm(start)
 
 	# myGraph.PrintGraph()
