a66365217b53f9ef0be7ea0136fe7089ff2693ce
Jin-Mac <taejin7824@gmail.com>
2019-12-05 22:17:41 +0900

    Add temp Priority Queue

--- Prob1753_ShortestPath.py
+++ Prob1753_ShortestPath.py
@@ -1,6 +1,77 @@
 # Prob1753_ShortestPath.py
 from math import inf
 
+class PriorityQueue():
+	def __init__(self):
+		self.queue = [ None, ]
+		self.size = 1
+
+
+	def push(self, data):
+		self.queue.append(data)
+		self.size = self.size + 1
+		me = self.size - 1
+		mom = me // 2
+		while (me != 1 and self.queue[mom] > self.queue[me]):
+			self.queue[mom], self.queue[me] = self.queue[me], self.queue[mom]
+			me = mom
+			mom = me // 2
+
+
+	def Heapify(self, me):
+		leftChild = me * 2
+		rightChild = me * 2 + 1
+
+		smaller = None
+	
+		if (leftChild >= self.size):
+			return
+		elif (rightChild >= self.size):
+			smaller = leftChild
+		else:
+			if (self.queue[leftChild] < self.queue[rightChild]):
+				smaller = leftChild
+			else:
+				smaller = rightChild
+
+			if (self.queue[smaller] < self.queue[me]):
+				self.queue[smaller], self.queue[me] = self.queue[me], self.queue[smaller]
+				self.Heapify(smaller)
+
+
+	def pop(self):
+		last = self.size - 1
+		self.queue[1], self.queue[last] = self.queue[last], self.queue[1]
+
+		self.size = self.size - 1
+
+		self.Heapify(1)
+
+		return self.queue.pop()
+		
+
+	def isEmpty(self) -> 'bool':
+		if self.queue: return False
+		else: return True
+
+
+	def PrintQueue(self):
+		print(self.queue)
+
+if (__name__ == '__main__'):
+	myQueue = PriorityQueue()
+	myQueue.push(3)
+	myQueue.push(5)
+	myQueue.push(1)
+	myQueue.push(2)
+	myQueue.push(6)
+	myQueue.PrintQueue()
+
+	for i in range(5):
+		print(myQueue.pop())
+		myQueue.PrintQueue()
+
+
 class Graph():
 	VISIT = 1
 	UNVISIT = 0
@@ -32,17 +103,23 @@ class Graph():
 			for currVertex in self.adjList:
 				if (visited[currVertex] is Graph.VISIT):
 					for adjVertex in self.adjList[currVertex]:
-						tempWeight = shortestPath[currVertex] + adjVertex[Graph.W]
-						if (shortestPath[adjVertex[Graph.V]] > tempWeight):
-							shortestPath[adjVertex[Graph.V]] = tempWeight
-							
-						if ((minPath is None) or \
-							(shortestPath[minPath] > shortestPath[adjVertex[Graph.V]])):
-							minPath = shortestPath[adjVertex[Graph.V]]
-			visited[minPath] = Graph.VISIT
+						adjVertexNum = adjVertex[Graph.V]
+						if (visited[adjVertexNum] is Graph.UNVISIT):
+							tempWeight = shortestPath[currVertex] + adjVertex[Graph.W]
+							if (shortestPath[adjVertexNum] > tempWeight):
+								shortestPath[adjVertexNum] = tempWeight
+								
+							if ((minPath is None) or \
+								(shortestPath[minPath] > shortestPath[adjVertexNum])):
+								minPath = adjVertexNum
+			if (minPath is not None):
+				visited[minPath] = Graph.VISIT
 
 		for short in shortestPath:
-			print(short)
+			if shortestPath[short] is not inf:
+				print(shortestPath[short])
+			else:
+				print('INF')
 
 if (__name__ == '__main__'):
 	vertexSize, edgeSize = map(int, input().split())
@@ -57,3 +134,12 @@ if (__name__ == '__main__'):
 	myGraph.DijkstraAlgorithm(start)
 
 	# myGraph.PrintGraph()
+
+	# myGraph = Graph(5)
+	# myGraph.InsertEdge(5, 1, 1)
+	# myGraph.InsertEdge(1, 2, 2)
+	# myGraph.InsertEdge(1, 3, 3)
+	# myGraph.InsertEdge(2, 3, 4)
+	# myGraph.InsertEdge(2, 4, 5)
+	# myGraph.InsertEdge(3, 4, 6)
+	# myGraph.DijkstraAlgorithm(1)	
