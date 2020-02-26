e06c7df0df0789de67c5b8b478e37e830d7af872
Taejin-Mac <taejin7824@gmail.com>
2020-02-26 23:57:04 +0900

    Solution Baekjoon 1065 C++

---
+++ C++/Baekjoon01065_Hansu.cpp
@@ -0,0 +1,34 @@
+// Baekjoon01065_Hansu.cpp
+#include <iostream>
+
+using namespace std;
+
+bool isHansu(int n) {
+	int a_prev = n % 10; n /= 10;
+	int a_next = n % 10;
+
+	int difference = a_next - a_prev;
+	while ( n != 0 ) {
+		n /= 10;
+		if ( difference != a_next - a_prev )
+			return false;
+
+		a_prev = a_next;
+		a_next = n % 10;;
+	}
+
+	return true;
+}
+
+int main(void) {
+	int n, cnt = 0;
+	
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		if ( isHansu(i) )
+			cnt++;
+
+	cout << cnt << endl;
+
+	return 0;
+}
