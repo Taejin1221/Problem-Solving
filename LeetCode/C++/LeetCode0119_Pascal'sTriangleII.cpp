6417598cbf0f14e198e6a02318e132d04d56d544
Jin-Desk <taejin7824@gmail.com>
2020-08-14 23:34:43 +0900

    Reduce Code LeetCode 119

--- LeetCode/C++/LeetCode0119_Pascal'sTriangleII.cpp
+++ LeetCode/C++/LeetCode0119_Pascal'sTriangleII.cpp
@@ -8,12 +8,8 @@ public:
 		pascal.push_back( 1 );
 
 		for ( int i = 1; i <= k; i++ ) {
-			int prevVal = pascal[0];
-			for ( int j = 1; j < i; j++ ) {
-				int temp = pascal[j];
-				pascal[j] = prevVal + pascal[j];
-				prevVal = temp;
-			}
+			for ( int j = i - 1; j > 0; j-- )
+				pascal[j] = pascal[j - 1] + pascal[j];
 			pascal.push_back( 1 );
 		}
 
