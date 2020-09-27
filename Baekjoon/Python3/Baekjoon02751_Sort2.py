abc576b0f5a1780c0fa44a2dab5ebafdb3be540b
JIN-DESK <taejin7824@gmail.com>
2020-09-27 20:00:38 +0900

    Baekjoon 2751 수 정렬하기 2 Solution with Python3

---
+++ Baekjoon/Python3/Baekjoon02751_Sort2.py
@@ -0,0 +1,48 @@
+# Baekjoon02751_Sort2.py
+def MergeSort( arr, left, right ):
+	if left >= right: return
+	else:
+		mid = ( left + right ) // 2
+		MergeSort( arr, left, mid )
+		MergeSort( arr, mid + 1, right )
+		Merge( arr, left, mid, right )
+
+def Merge( arr, left, mid, right ):
+	leftCopy = arr[ left : mid + 1 ]
+	rightCopy = arr[ mid + 1 : right + 1 ]
+
+	lenLeft, lenRight = len( leftCopy ), len( rightCopy )
+	idxLeft, idxRight, idxOrigin = 0, 0, left
+
+	while ( ( idxLeft < lenLeft ) and ( idxRight < lenRight ) ):
+		leftVal, rightVal = leftCopy[idxLeft], rightCopy[idxRight]
+
+		if leftVal < rightVal:
+			arr[idxOrigin] = leftVal
+			idxLeft += 1
+		else:
+			arr[idxOrigin] = rightVal
+			idxRight += 1
+
+		idxOrigin += 1
+
+	while ( idxLeft < lenLeft ):
+		arr[idxOrigin] = leftCopy[idxLeft]
+		idxOrigin += 1; idxLeft += 1
+
+	while ( idxRight < lenRight ):
+		arr[idxOrigin] = rightCopy[idxRight]
+		idxOrigin += 1; idxRight += 1
+
+import sys
+input = sys.stdin.readline
+
+n = int( input() )
+arr = []
+for i in range( n ):
+	arr.append( int( input() ) )
+
+MergeSort( arr, 0, n - 1 )
+
+for i in range( n ):
+	print( arr[i] )
