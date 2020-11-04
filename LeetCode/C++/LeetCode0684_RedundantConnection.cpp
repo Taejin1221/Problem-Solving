85731258d63216b274bd310b0a0cbd56baad2c04
Jin-MacBook <taejin7824@gmail.com>
2020-11-04 16:10:00 +0900

    LeetCode 684 Algorithm(Union Find Tree) C++ Solution

--- LeetCode/C++/LeetCode0684_RedundantConnection.cpp
+++ LeetCode/C++/LeetCode0684_RedundantConnection.cpp
@@ -1,5 +1,6 @@
 // LeetCode0684_RedundantConnection.cpp
 // https://leetcode.com/problems/redundant-connection/
+// method 1 : DFS
 class Solution {
 private:
 	bool hasCycle( int me, int mom, vector<int>& removeEdge, vector<vector<int>>& adjList, vector<bool>& visited ) {
@@ -55,4 +56,56 @@ public:
 
 		return removeEdge;
 	}
+};
+
+// method 2 : Union Find
+class Solution {
+private:
+	int GetRoot( int target, vector<int>& parents ) {
+		if ( parents[target] == target )
+			return target;
+		else
+			return GetRoot( parents[target], parents );
+	}
+
+	void Union( int u, int v, vector<int>& parents ) {
+		if ( u < v )
+			parents[v] = u;
+		else
+			parents[u] = v;
+	}
+
+public:
+	vector<int> findRedundantConnection( vector<vector<int>>& edges ) {
+		int numNodes = edges.size();
+
+		vector<int> removeEdge;
+		for ( int i = edges.size() - 1; i >= 0; i-- ) {
+			removeEdge = edges[i];
+
+			vector<int> parents ( numNodes + 1 );
+			for ( int j = 1; j <= numNodes; j++ )
+				parents[j] = j;
+
+			bool isCycle = false;
+			for ( int currEdge = 0; currEdge < edges.size(); currEdge++ ) {
+				if ( currEdge != i ) {
+					int u = edges[currEdge][0], v = edges[currEdge][1];
+					int uRoot = GetRoot( u, parents ), vRoot = GetRoot( v, parents );
+					if ( uRoot == vRoot ) {
+						isCycle = true;
+						break;
+					} else {
+						Union( uRoot, vRoot, parents );
+					}
+				}
+			}
+
+			if ( !isCycle )
+				break;
+		}
+
+		return removeEdge;
+	}
+
 };
