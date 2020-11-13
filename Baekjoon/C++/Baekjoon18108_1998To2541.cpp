6ed0161252c26fd236d16e802c2698fc58c56f58
Jin-MacBook <taejin7824@gmail.com>
2020-11-14 00:26:33 +0900

    Baekjoon 18109(Math, Bronze) C++ Solution

---
+++ Baekjoon/C++/Baekjoon18108_1998To2541.cpp
@@ -0,0 +1,14 @@
+// Baekjoon18108_1998To2541.cpp
+// https://www.acmicpc.net/problem/18108
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+
+	cout << n - 543 << '\n';
+
+	return 0;
+}
