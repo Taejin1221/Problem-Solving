d174b74b854812d0095e420e0fdf83594eb0d9fe
Jin-MacBook <taejin7824@gmail.com>
2020-12-30 23:18:30 +0900

    Add 2 different and fast C++ Solution

--- Baekjoon/C++/Baekjoon02225_SumDecomposition.cpp
+++ Baekjoon/C++/Baekjoon02225_SumDecomposition.cpp
@@ -14,11 +14,30 @@ int main(void) {
 	for ( int i = 1; i <= K; i++ )
 		dp[0][i] = 1;
 
+	// Method 1: Sum of All
 	for ( int i = 1; i <= N; i++ ) {
 		dp[i][1] = 1;
 		for ( int j = 2; j <= K; j++ )
 			for ( int k = 0; k <= i; k++ )
 				dp[i][j] =  (dp[i][j] + dp[k][j - 1]) % MOD;
+	}
+
+	cout << dp[N][K] << '\n';
+
+	// Method 2: Using prefix sum
+	for ( int i = 1; i <= N; i++ ) {
+		dp[i][1] = dp[i - 1][1] + 1;
+		for ( int j = 2; j <= K; j++ )
+			dp[i][j] = ( dp[i - 1][j] + dp[i][j - 1] ) % MOD;
+	}
+
+	cout << (dp[N][K] - dp[N - 1][K] + MOD) % MOD << '\n';
+
+	// Method 3: Using value caculated already
+	for ( int i = 1; i <= N; i++ ) {
+		dp[i][1] = 1;
+		for ( int j = 2; j <= K; j++ )
+			dp[i][j] = ( dp[i - 1][j] + dp[i][j - 1] ) % MOD;
 	}
 
 	cout << dp[N][K] << '\n';
