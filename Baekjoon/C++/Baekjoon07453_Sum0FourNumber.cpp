a3d0b53552713a2daf78908c950496aea46d44af
Jin-MacBook <taejin7824@gmail.com>
2021-01-11 22:04:27 +0900

    Add Review

--- Baekjoon/C++/Baekjoon07453_Sum0FourNumber.cpp
+++ Baekjoon/C++/Baekjoon07453_Sum0FourNumber.cpp
@@ -1,5 +1,24 @@
 // Baekjoon07453_Sum0FourNumber.cpp
 // https://www.acmicpc.net/problem/7453
+
+/* Review
+ * Date
+ 01/11/2021
+
+ * Comment
+ 단순할 것 같았지만 굉장히 까다로운 문제였다. 처음엔 c + d의 값들을 map에 넣어 찾았지만 시간 초과였다. 그래서 그 다음으론 set에 넣어 찾았지만 이번에도 시간 초과였다.
+ 그래서 마지막으로 lower_bound, upper_bound로 구현하였더니 드디어 맞았다. 사실 이전엔 lower_bound, upper_bound를 사용한적이 거의 없어 잘 몰랐었다.
+ 하지만 이번 문제를 풀음으로써 lower_bound에 대한 자신감이 생겼다.
+
+ * Solution
+ 특정 value와 더했을 때 0이 되는 값은 -value이다. 따라서 이 문제는 a와 b의 모든 더하기 경우의 수를 구하고, c와 d의 모든 더하기 경우를 구한 뒤,
+ a+b의 value가 c+d에 -value로 들어있으면 count를 증가시키면된다. 이때 단순 탐색으로 구현하면 O(n^2)으로 결국 O(n^2 * n^2)이다.
+ 그러므로 정렬한 뒤 이진탐색으로 값을 찾아주면 정렬과 탐색은 O(n^2 * log n^2)이다.
+ 1. a + b와 c + d를 구한다.
+ 2. c + d를 정렬한다.
+ 3. a + b 배열에 있는 값들에 -를 곱한 값을 c + d에 이진 탐색으로 찾아준다.
+*/
+
 #include <iostream>
 
 #include <vector>
