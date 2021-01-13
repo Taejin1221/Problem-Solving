88ff31babbe34104cfd134facd5eba0ef0c58ebc
JIN-DESK <taejin7824@gmail.com>
2021-01-14 01:00:38 +0900

    IUPC B Solution

---
+++ Baekjoon/C++/IUPC_B.cpp
@@ -0,0 +1,35 @@
+// IUPC_B.cpp
+#include <iostream>
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> arr( n );
+	for ( int i = 0; i < n; i++ )
+		cin >> arr[i];
+	sort( arr.begin(), arr.end() );
+
+	while ( m-- ) {
+		int value;
+		cin >> value;
+
+		int idx = lower_bound( arr.begin(), arr.end(), value ) - arr.begin();
+
+		if ( idx < n && arr[idx] == value )
+			cout << idx;
+		else
+			cout << -1;
+		cout << '\n';
+	}
+
+	return 0;
+}
