74fd770698927c88e3c469b5a77e342e722123e1
Jin-MacBook <taejin7824@gmail.com>
2021-02-11 04:35:51 +0900

    Baekjoon 16236(BFS, Priority Queue, Gold) C++ Solution

--- Baekjoon/C++/Baekjoon16236_BabyShark.cpp
+++ Baekjoon/C++/Baekjoon16236_BabyShark.cpp
@@ -1,125 +1,96 @@
 // Baekjoon16236_BabyShark.cpp
 // https://www.acmicpc.net/problem/16236
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
 
-#define ABS( x ) (( ( x ) < 0 ) ? ( -(x) ) : x)
+#include <queue>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-struct Position {
-	int row, col;
-	Position( ): row(0), col(0) {}
-	Position( int r, int c ): row(r), col(c) {}
+const int rowChange[4] = { -1, 0, 0, 1 }, colChange[4] = { 0, -1, 1, 0 };
 
-	int operator-( const Position& p2 ) {
-		return ( ABS(row - p2.row) + ABS(col - p2.col) );
-	}
-};
+typedef pair<int, int> pii;
 
 struct Node {
-	Position pos;
-	int distance;
-	Node( ): pos(), distance(0) {}
-	Node( int r, int c, int d ): pos(r, c), distance(d) {}
-	Node( Position p, int d ): pos(p), distance(d) {}
+	pii pos;
+	int time;
+	Node( pii p, int t ): pos( p ), time( t ) { }
 
-	bool operator<( const Node& n2 ) {
-		if ( pos.row != n2.pos.row )
-			return pos.row < n2.pos.row;
-		else
-			return pos.col < n2.pos.col;
+	bool operator<( const Node& n2 ) const {
+		if ( time != n2.time )
+			return time > n2.time;
+		else {
+			if ( pos.first != n2.pos.first )
+				return pos.first > n2.pos.first;
+			else
+				return pos.second > n2.pos.second;
+		}
 	}
 };
-
-Position shark;
-int n, second, sharkSize = 2;
-int map[20][20];
-
-Position bfs( Position curr );
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
+	int n;
 	cin >> n;
 
+	pii baby;
+	int ocean[20][20];
 	for ( int i = 0; i < n; i++ ) {
 		for ( int j = 0; j < n; j++ ) {
-			cin >> map[i][j];
-			if ( map[i][j] == 9 ) {
-				shark.row = i, shark.col = j;
-				map[i][j] = 0;
+			cin >> ocean[i][j];
+
+			if ( ocean[i][j] == 9 ) {
+				baby = { i, j };
+				ocean[i][j] = 0;
 			}
 		}
 	}
 
-	int fish = 0;
-	while ( true ) {
-		shark = bfs( shark );
-		if ( shark.row == -1 && shark.col == -1 )
-			break;
-		map[shark.row][shark.col] = 0;
+	int second = 0, sharkSize = 2, fishNum = 0;
+	while ( !( baby.first == -1 && baby.second == -1 ) ) {
+		priority_queue<Node> pq;
+		bool visited[20][20] = { false, };
 
-		fish++;
-		if ( fish == sharkSize ) {
-			fish = 0;
+		pq.push( Node( baby, 0 ) );
+		visited[baby.first][baby.second] = true;
+
+		pii nextPos = { -1, -1 };
+		while ( !pq.empty() ) {
+			Node curr = pq.top(); pq.pop();
+
+			pii currPos = curr.pos;
+			int fishPos = ocean[currPos.first][currPos.second]; // fish size on current position
+			if ( fishPos != 0 && fishPos < sharkSize ) {
+				nextPos = currPos;
+				ocean[nextPos.first][nextPos.second] = 0;
+				fishNum++;
+				second += curr.time;
+				break;
+			}
+
+			// bfs
+			for ( int i = 0; i < 4; i++ ) {
+				pii next = { currPos.first + rowChange[i], currPos.second + colChange[i] };
+				if ( ( 0 <= next.first && next.first < n ) && ( 0 <= next.second && next.second < n ) ) {
+					if ( !visited[next.first][next.second] && ocean[next.first][next.second] <= sharkSize ) {
+						visited[next.first][next.second] = true;
+						pq.push( Node( next, curr.time + 1 ) );
+					}
+				}
+			}
+		}
+		
+		baby = nextPos;
+		if ( fishNum == sharkSize ) {
 			sharkSize++;
+			fishNum = 0;
 		}
 	}
 
 	cout << second << '\n';
 
 	return 0;
-}
-
-Position bfs( Position start ) {
-	queue<Node> q;
-	vector<Node> list;
-	bool visited[20][20] = { false, };
-
-	int minDistance = 100;
-	q.push( Node( start, 0 ) );
-	visited[start.row][start.col] = true;
-	while( !q.empty() ) {
-		Node currNode = q.front(); q.pop();
-
-		Position currPos = currNode.pos;
-		if ( map[currPos.row][currPos.col] && map[currPos.row][currPos.col] < sharkSize ) {
-			if ( minDistance < currNode.distance )
-				break;
-			else {
-				minDistance = currNode.distance;
-				list.push_back( currNode );
-			}
-		}
-
-		Position nexts[4] = {
-			Position( currPos.row - 1, currPos.col ),
-			Position( currPos.row, currPos.col - 1 ),
-			Position( currPos.row, currPos.col + 1),
-			Position( currPos.row + 1, currPos.col )
-		};
-
-		for ( Position& next : nexts ) {
-			int row = next.row, col = next.col;
-			if ( (0 <= row && row < n) && (0 <= col && col < n) ) {
-				if ( !visited[row][col] ) {
-					visited[row][col] = true;
-					if ( map[row][col] <= sharkSize )
-						q.push( Node( row, col , currNode.distance + 1 ) );
-				}
-			}
-		}
-	}
-
-	if ( list.size() ) {
-		sort( list.begin(), list.end() );
-		second += list[0].distance;
-
-		return list[0].pos;
-	}
-	else
-		return Position( -1, -1 );
 }
