d17c434d271adeaa3f3cae060b0dc8ce7c869ec7
Taejin-Mac <taejin7824@gmail.com>
2021-09-01 18:12:29 +0900

    Add Baekjoon(Math, Exponential by Squaring, Number Theory, Gold) C++ Solution

---
+++ Baekjoon/Baekjoon13172.cpp
@@ -0,0 +1,45 @@
+// Baekjoon13172.cpp
+// https://www.acmicpc.net/problem/13172
+#include <iostream>
+
+#define MOD 1'000'000'007
+
+using namespace std;
+
+typedef long long ll;
+
+ll myPow(ll x, ll y) {
+	if (y == 0)
+		return 1;
+	else if (y == 1)
+		return x;
+	else if (y == 2)
+		return (x * x) % MOD;
+
+	ll result = myPow(x, y / 2);
+	result = result * result % MOD;
+	if (y % 2)
+		result = result * x % MOD;
+	return result;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	ll ans = 0;
+	while (n--) {
+		ll a, b;
+		cin >> b >> a;
+
+		ll result = a * myPow(b, MOD - 2) % MOD;
+		ans = (ans + result) % MOD;
+	}
+
+	cout << ans << '\n';
+
+	return 0;
+}
