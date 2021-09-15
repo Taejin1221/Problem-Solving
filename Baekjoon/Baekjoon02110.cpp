d7a0060a52aba7d3793247425dd4acc8724a8b8e
Taejin-Mac <taejin7824@gmail.com>
2021-09-15 19:24:36 +0900

    Fix Wrong Answer(Baekjoon, 2110)

--- Baekjoon/Baekjoon02110.cpp
+++ Baekjoon/Baekjoon02110.cpp
@@ -6,12 +6,12 @@
 using namespace std;
 
 int n, c;
-int homes[200000];
+int homes[200'000];
 
-int Router( int dis ) {
+int Router(int dis) {
 	int sum = 1, prevIdx = 0;
-	for ( int currIdx = 1; currIdx < n; currIdx++ ) {
-		if ( (homes[currIdx] - homes[prevIdx]) >= dis ) {
+	for (int currIdx = 1; currIdx < n; currIdx++) {
+		if (dis <= homes[currIdx] - homes[prevIdx]) {
 			prevIdx = currIdx;
 			sum++;
 		}
@@ -26,20 +26,25 @@ int main(void) {
 	
 	cin >> n >> c;
 	
-	for ( int i = 0; i < n; i++ )
+	for (int i = 0; i < n; i++)
 		cin >> homes[i];
-	sort( homes, homes + n );
+	sort(homes, homes + n);
 
+	int ans = 0;
 	int left = 1, right = homes[n - 1] - homes[0];
-	while ( left + 1 < right ) {
-		int mid = ( right + left ) / 2;
-		if ( Router( mid ) >= c )
-			left = mid;
+	while (left <= right) {
+		int mid = (left + right) / 2;
+
+		int result = Router(mid);
+		if (c <= result) {
+			left = mid + 1;
+			ans = max(ans, mid);
+		}
 		else
-			right = mid;
+			right = mid - 1;
 	}
 
-	cout << left << '\n';
+	cout << ans << '\n';
 
 	return 0;
 }
