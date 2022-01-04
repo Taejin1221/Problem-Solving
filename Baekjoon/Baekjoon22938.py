3d4657da24d21d4ccafc03530d0e811f5546de5d
wrathlion <taejin7824@gmail.com>
2022-01-04 19:22:50 +0900

    S BOJ 22938 B Py

---
+++ Baekjoon/Baekjoon22938.py
@@ -0,0 +1,9 @@
+x1, y1, r1 = map(int, input().split())
+x2, y2, r2 = map(int, input().split())
+
+disSquare = (x1 - x2) ** 2 + (y1 - y2) ** 2
+radiusSquare = (r1 + r2) ** 2
+if disSquare < radiusSquare:
+    print('YES')
+else:
+    print('NO')
