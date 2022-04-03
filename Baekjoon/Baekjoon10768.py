485dc7f78906c68e91c469502517c21bf2211285
wrathlion <taejin7824@gmail.com>
2022-04-03 23:43:43 +0900

    S BOJ 10768(Impl) B Py

---
+++ Baekjoon/Baekjoon10768.py
@@ -0,0 +1,14 @@
+m = int(input())
+d = int(input())
+
+if m < 2:
+    print('Before')
+elif m > 2:
+    print('After')
+else:
+    if d < 18:
+        print('Before')
+    elif d > 18:
+        print('After')
+    else:
+        print('Special')
