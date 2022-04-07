034bd94a05c6b52d5f54b611e53ca1d5cf18bf70
wrathlion <taejin7824@gmail.com>
2022-04-07 13:27:11 +0900

    S BOJ 14264(Math) B Py

---
+++ Baekjoon/Baekjoon14264.cpp
@@ -0,0 +1,16 @@
+// Baekjoon14264.cpp
+// https://www.acmicpc.net/problem/14264
+#include <cstdio>
+
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    double l;
+    scanf("%lf", &l);
+
+    printf("%.9lf\n", sqrt(3) * l * l / 4);
+
+    return 0;
+}
