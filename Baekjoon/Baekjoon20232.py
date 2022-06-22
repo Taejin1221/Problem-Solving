996d88b686d227ea09c3a7bdec0df1aeee6be053
wrathlion <taejin7824@gmail.com>
2022-06-22 23:56:55 +0900

    S BOJ 20232(Impl) B Py

---
+++ Baekjoon/Baekjoon20232.py
@@ -0,0 +1,3 @@
+n = int(input())
+table = ['ITMO','SPbSU','SPbSU','ITMO','ITMO','SPbSU','ITMO','ITMO', 'ITMO', 'ITMO', 'ITMO', 'PetrSU, ITMO', 'SPbSU', 'SPbSU', 'ITMO', 'ITMO', 'ITMO', 'ITMO', 'SPbSU', 'ITMO','ITMO','ITMO','ITMO','SPbSU','ITMO']
+print(table[n - 1995])
