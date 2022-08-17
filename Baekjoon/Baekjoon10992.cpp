8b26c9e045c7bbc5b970643c9cf284d7d08947a8
JIN-DESK <taejin7824@gmail.com>
2022-08-18 01:28:23 +0900

    S BOJ 10992(Impl) B C++

---
+++ Baekjoon/Baekjoon10992.cpp
@@ -0,0 +1,37 @@
+// Baekjoon10992.cpp
+// https://www.acmicpc.net/problem/10992
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+
+	if (n == 1) {
+		cout << '*';
+
+		return 0;
+	}
+
+	for (int i = 0; i < n - 1; i++)
+		cout << ' ';
+	cout << '*';
+	cout << '\n';
+
+	for (int i = 2; i <= n - 1; i++) {
+		for (int j = 0; j < n - i; j++)
+			cout << ' ';
+		cout << '*';
+		for (int j = 0; j < (i - 1) * 2 - 1; j++)
+			cout << ' ';
+		cout << '*';
+		cout << '\n';
+	}
+
+	for (int i = 0; i < n * 2 - 1; i++)
+		cout << '*';
+	cout << '\n';
+
+	return 0;
+}
